5ffe43890c6255ac2404f8be966ae2a17fdf1a61
procub3r <sripradp@protonmail.com>
2021-07-09 19:48:59 +0530

    Create pecs.h with the Registry struct declaration
    Also typedef `Entity`, `ComponentType` and `Signature` to `unsigned int`

---
+++ include/pecs.h
@@ -0,0 +1,19 @@
+#ifndef PECS_H
+#define PECS_H
+
+#include <stdlib.h>
+
+typedef unsigned int Entity, ComponentType, Signature;
+
+typedef struct Registry {
+    // Entities related:
+    Entity total_entities;
+    Signature* signatures;
+
+    // Components related:
+    ComponentType total_component_types;
+    size_t* component_sizes;
+    void** components;
+} Registry;
+
+#endif  // PECS_H
